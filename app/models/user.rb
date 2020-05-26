1596b84aebdb7d818505cd3b91b2bbc383a89682
tak-fomalhaut <star227clover@gmail.com>
2020-05-26 12:27:55 +0900

    deviseインストール完了
    インストール
    マイグレーションファイルの修正
    userとdeviseモデルの重複の調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-#   has_many :comments, dependent: :destroy
-#   has_many :favorites, dependent: :destroy
-# - has_many :todo_lists
-# - has_many :user_evaluations
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  #   has_many :comments, dependent: :destroy
+  #   has_many :favorites, dependent: :destroy
+  # - has_many :todo_lists
+  # - has_many :user_evaluations
   has_many :seller_items, foreign_key: "seller_id", class_name: "items"
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "items"
-# - has_one :point
+  # - has_one :point
   has_one :profile, dependent: :destroy
-# - has_one :sns_authentication, dependent: :destroy
-# - has_one :sending_destination, dependent: :destroy
+  # - has_one :sns_authentication, dependent: :destroy
+  # - has_one :sending_destination, dependent: :destroy
   has_one :credit_card, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "items#index"
   resources :users
   resources :items do
