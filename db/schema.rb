bebd248668869a7cfbb5425d3209942c6cd8d9b2
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-27 15:43:53 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_081156) do
+ActiveRecord::Schema.define(version: 2020_05_26_075318) do
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "card_number", null: false
-    t.integer "expiration_year", null: false
-    t.integer "expiration_month", null: false
-    t.integer "security_code", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id"
+    t.string "card_id"
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
