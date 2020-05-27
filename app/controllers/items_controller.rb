4310c34a2724a6d179f9628062b0738e48942c51
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-05-27 12:50:40 +0900

    updatedTopPage
    商品が購入されると、トップページから表示されないように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
-    @newestItems = Item.last(3)
-    @item_img = ItemImg.all
+
     allItems = Item.all
-    last3deleted = allItems[0..((allItems.length)-4)]
+    selling = []
+
+    # 未購入商品を取り出して配列sellingに入れる
+    allItems.each do |product|
+      if product.users_id.present?
+        #何もしない
+      else  
+        selling << product
+      end
+    end
+
+    # 新着商品の最新の３つを表示
+    @newestItems = selling.last(3)
+    # item_imgテーブルから上記と適した画像を取得
+    @item_img = ItemImg.all
+
+    # ピックアップ商品を3つ表示（3つの未購入かつ最新商品以外）
+    last3deleted = selling[0..((selling.length)-4)]
+    # リロードすると違う商品が表示されるよう、配列をシャッフル
     random = last3deleted.shuffle
     @pickupItems = random.take(3)
   end
