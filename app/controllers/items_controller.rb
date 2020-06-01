79afe7e5bde9c19a023dad1ee619578a0a7426c1
tak-fomalhaut <star227clover@gmail.com>
2020-06-01 11:45:42 +0900

    エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -60,7 +60,7 @@ class ItemsController < ApplicationController
     # 購入する商品を引っ張ってきます。
     @item = Item.find(params[:id])
     # 商品ごとに複数枚写真を登録できるので、一応全部持ってきておきます。
-    @image = @item.item_imgs[0].url.url.to_s
+    @image = @item.item_imgs[0].url.to_s
 
     # まずはログインしているか確認
     if user_signed_in?
