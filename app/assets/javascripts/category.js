6dce29c032f1996f5bbdfd50ce4488e741964c50
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-02 12:47:00 +0900

    親カテゴリー表示・非表示
    _header.html.hamlにclassをつけて、そこにカテゴリーを追加（表示）させています。

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,24 +1,25 @@
 $(function() {
   // 子カテゴリーを追加するための処理です。
   function buildParentHTML(parent){
-    let html =`<a class="child_category" id="${parent.id}" 
+    let html =`<a class="parent_category" id="${parent.id}" 
                 href="/category/${parent.id}">${parent.name}</a>`;
-    $("#TPcategoryBtn").append(html);
+    $(".parents_list").append(html);
   }
   
   $("#TPcategoryBtn").on("mouseover", function() {
     $.ajax({
       type: 'GET',
-      url: '/',//とりあえずここでは、newアクションに飛ばしてます
-      // data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
+      url: '/',
       dataType: 'json'
     })
       .done(function(parents) {
-        console.log(parents)
-        $('.child_category').remove();
-        parents.forEach(function (parent) {//帰ってきた子カテゴリー（配列）
-          buildParentHTML(parent);//HTMLにして
+        $(".parent_category").remove();
+        parents.forEach(function (parent) {
+          buildParentHTML(parent);
         })
+        $(".TP-header__navi__left--links").on("mouseout", function () {
+          $(".parent_category").remove();
+        });
       });
   });
 });
