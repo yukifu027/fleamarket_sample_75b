b9a31d181fdfd2bf05ead564152f9b15870fe187
tak-fomalhaut <star227clover@gmail.com>
2020-06-02 16:27:31 +0900

    css調整

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,31 +1,31 @@
 // $(function() {
 
 
-//   // 子カテゴリーを追加するための処理です。
-//   function buildChildHTML(child){
-//     var html =`<a class="child_category" id="${child.id}" 
-//                 href="/category/${child.id}">${child.name}</a>`;
-//     return html;
-//   }
+  // 子カテゴリーを追加するための処理です。
+  // function buildChildHTML(child){
+  //   var html =`<a class="child_category" id="${child.id}" 
+  //               href="/category/${child.id}">${child.name}</a>`;
+  //   return html;
+  // }
 
-//   $(".parent_category").on("mouseover", function() {
-//     var id = this.id//どのリンクにマウスが乗ってるのか取得します
-//     $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
-//     $('#' + id).addClass("now-selected-red");//赤色のcssのためです
-//     $(".child_category").remove();//一旦出ている子カテゴリ消します！
-//     $(".grand_child_category").remove();//孫、てめえもだ！
-//     $.ajax({
-//       type: 'GET',
-//       url: '/category/new',//とりあえずここでは、newアクションに飛ばしてます
-//       data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
-//       dataType: 'json'
-//     }).done(function(children) {
-//       children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
-//         var html = buildChildHTML(child);//HTMLにして
-//         $(".children_list").append(html);//リストに追加します
-//       })
-//     });
-//   });
+  // $(".parent_category").on("mouseover", function() {
+  //   var id = this.id//どのリンクにマウスが乗ってるのか取得します
+  //   $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
+  //   $('#' + id).addClass("now-selected-red");//赤色のcssのためです
+  //   $(".child_category").remove();//一旦出ている子カテゴリ消します！
+  //   $(".grand_child_category").remove();//孫、てめえもだ！
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/category/new',//とりあえずここでは、newアクションに飛ばしてます
+  //     data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
+  //     dataType: 'json'
+  //   }).done(function(children) {
+  //     children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+  //       var html = buildChildHTML(child);//HTMLにして
+  //       $(".children_list").append(html);//リストに追加します
+  //     })
+  //   });
+  // });
 
 //   // 孫カテゴリを追加する処理です　基本的に子要素と同じです！
 //   function buildGrandChildHTML(child){
@@ -72,7 +72,7 @@
 
 
 $(function() {
-  // 子カテゴリーを追加するための処理です。
+  // 親カテゴリーを追加するための処理です。
   function buildParentHTML(parent){
     let html =`<a class="li parent_category" id="${parent.id}" 
                 href="/category/${parent.id}">${parent.name}</a>`;
@@ -96,5 +96,29 @@ $(function() {
           $(".parent_category").remove();
         });
       });
+  });
+  function buildChildHTML(child){
+    var html =`<a class="li child_category" id="${child.id}" 
+                href="/category/${child.id}">${child.name}</a>`;
+    return html;
+  }
+
+  $(".parent_category").on("mouseover", function() {
+    var id = this.id//どのリンクにマウスが乗ってるのか取得します
+    $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
+    $('#' + id).addClass("now-selected-red");//赤色のcssのためです
+    $(".child_category").remove();//一旦出ている子カテゴリ消します！
+    $(".grand_child_category").remove();//孫、てめえもだ！
+    $.ajax({
+      type: 'GET',
+      url: '/category/new',//とりあえずここでは、newアクションに飛ばしてます
+      data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
+      dataType: 'json'
+    }).done(function(children) {
+      children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+        var html = buildChildHTML(child);//HTMLにして
+        $(".children_list").append(html);//リストに追加します
+      })
+    });
   });
 }); 
