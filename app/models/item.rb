5c96ab4111c2d889ce87601ded9b72c5f91ddaa9
oimokun <shiriki0811@gmail.com>
2020-06-03 11:20:33 +0900

    複数画像投稿最終

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,12 @@ class Item < ApplicationRecord
   # has_many :favorites
   has_many :item_imgs, dependent: :destroy
   # has_one :user_evaluation
-  # belongs_to :category
+  belongs_to :category
   # belongs_to_active_hash :size
   # belongs_to_active_hash :postage_payer
   # belongs_to_active_hash :preparation_day
   # belongs_to_active_hash :postage_type
-  # belongs_to :brand
+  belongs_to :brand
   # belongs_to :seller, class_name: "User"
   # belongs_to :buyer, class_name: "User"
 
@@ -17,4 +17,7 @@ class Item < ApplicationRecord
   
   accepts_nested_attributes_for :item_imgs, allow_destroy: true
 
+
+  # validates :item_img_id, :name, :introduction, :category_id, :item_condition, :postage_payer_id, :prefecture_code, :preparation_day_id, :price, presence: true
+  
 end
