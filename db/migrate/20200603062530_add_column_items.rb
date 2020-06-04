580a71808d53a2f0e81572b833a20dcbbdc77d6f
tak-fomalhaut <star227clover@gmail.com>
2020-06-04 18:07:42 +0900

    重複migrationmfairu削除

--- db/migrate/20200603062530_add_column_items.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :postage_payer_id, :integer, null: false
-    add_column :items, :preparation_day_id, :integer, null: false
-    add_column :items, :category_id, :integer, null: false
-    add_column :items, :item_condition_id, :integer, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_03_062530) do
-
+ActiveRecord::Schema.define(version: 2020_06_03_051946) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
