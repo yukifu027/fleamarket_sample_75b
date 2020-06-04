e4f9cb7da69c83b78fc6c08961032c16612cdf10
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-04 15:07:47 +0900

    リファクタリング等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -87,6 +87,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     # 商品ごとに複数枚写真を登録できるので、一応全部持ってきておきます。
     @image = @item.item_imgs[0].url.to_s
+    @sending_destination = SendingDestination.find_by(user_id: current_user.id)
 
     # まずはログインしているか確認
     if user_signed_in?
