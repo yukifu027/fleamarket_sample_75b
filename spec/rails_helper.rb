354f1acd9a18ba56da9f727a662805f765e40fe5
kou021 <wxs2410@gmail.com>
2020-06-05 17:26:06 +0900

    商品出品/編集・テスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #下記の記述を追加
+  config.include FactoryBot::Syntax::Methods
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
