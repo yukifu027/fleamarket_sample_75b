c887987f350515bc4c9ef20ea8ddca2b17086992
ronjeff0816 <ronjeffersonlotino@gmail.com>
2020-06-10 11:18:14 +0900

    Update items_controller.rb
    カテゴリー編集用のコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,6 +61,25 @@ class ItemsController < ApplicationController
   end
 
   def edit
+
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children.name
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren.id
+    end
+
   end
 
   def update
